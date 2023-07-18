2c051bd39098e623011102115a5de4f282379263
Victor Quarracino <victorquarra@gmail.com>
2023-07-18 17:40:48 -0300

    preentrega2

--- js/script.js
+++ js/script.js
@@ -1,43 +1,68 @@
 //Calcular costo total de productos y/o servicios seleccionados por el usuario.
-//En pagina de una empresa que hace renders 3D, elegir si quiero comprar un render fijo, una axonometria, o una animacion.
+//En pagina de una empresa que hace renders 3D, elegir si quiero comprar un render fijo, una axonometria, o una animacion y cuantos quiere.
 
 //Declaro las variables a utilizar
-const render = 40000;
-const axonometria = 60000;
-const animacion = 200000;
-let carrito = "";
-let nuevaCompra = false;
-let producto = prompt("¿Que desea comprar?: render, axonometria, animacion");
-let cantidad = parseInt(prompt(`¿Cuantos desea comprar?`));
 
-//Declaro las funcion 1
-
-function compraTotal(producto, cantidad) {
-  do {
-    seleccionarProducto(producto, cantidad);
-    nuevaCompra = confirm("¿Desea comprar algo mas?");
-    if (nuevaCompra) {
-      producto = prompt("¿Que desea comprar?: render, axonometria, animacion");
-      cantidad = parseInt(prompt(`¿Cuantos desea comprar?`));
-    }
-  } while (nuevaCompra);
-}
-//Declaro las funcion 2
-function seleccionarProducto(producto, cantidad) {
-  if (producto.toLowerCase() == "render") {
-    resultado = (cantidad * render);
-    carrito = `Del producto ${producto} compraste ${cantidad}. Da un total de ${resultado} pesos`;
-  } else if (producto.toLowerCase() == "axonometria") {
-    resultado = (cantidad * axonometria);
-    carrito = `Del producto ${producto} compraste ${cantidad}. Da un total de ${resultado} pesos`;
-  } else if (producto.toLowerCase() == "animacion"){
-    resultado = (cantidad * animacion);
-    carrito = `Del producto ${producto} compraste ${cantidad}. Da un total de ${resultado} pesos`;
-  } else {
-    alert("No vendemos ese producto")
+const productos = [
+  {
+    id : 1,
+    tipoProducto : "render",
+    valor : 40000,
+  },
+  {
+    id : 2,
+    tipoProducto : "axonometria",
+    valor : 60000,
+  },
+  {
+    id : 3,
+    tipoProducto : "animacion",
+    valor : 200000,
   }
-  document.write(`${carrito}<br>`);
-}
-//Llamo a la funcion
-compraTotal(producto, cantidad);
+];
 
+let sumadorDeCompras = [];
+
+function productoDelListado() {
+  let elegido = prompt("¿Que desea comprar?: render, axonometria, animacion");
+  producto = productos.find((p) => p.tipoProducto.toLowerCase() === elegido.toLowerCase());
+}
+
+function sumarProducto() {
+  if (producto) {
+    let cantidad = parseInt(prompt(`¿Cuantos desea comprar?`));
+    sumadorDeCompras.push({
+      producto: producto.tipoProducto,
+      cantidad: cantidad,
+      subtotal: producto.valor * cantidad
+    });
+  } else {
+    alert("Error. Intente de nuevo");
+  }
+}
+
+function confirmarCompra() {
+  while (true) {
+    productoDelListado();
+    sumarProducto();
+
+    if (!confirm("¿Desea comprar algo mas?")) {
+      break;
+    }
+  }
+}
+
+function compraTotal() {
+  console.log("Acumulado:");
+  sumadorDeCompras.forEach((item) => {
+    console.log(`- ${item.cantidad} ${item.producto}: ${item.subtotal}`);
+  });
+
+  let total = sumadorDeCompras.reduce((sum, item) => sum + item.subtotal, 0);
+  console.log(`Total: ${total}`);
+}
+
+
+confirmarCompra();
+
+compraTotal();
